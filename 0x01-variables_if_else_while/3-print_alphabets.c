da3af29383d34761fd67d397d09149e48de56701
Akinola <akinolatofunmi04@gmail.com>
2022-06-03 16:30:24 -0700

    Test

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphA;
+	char alpha;
+
+	if (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	if (alphA = 'A'; alphA <= 'Z'; alphA++)
+	{
+		putchat(alphA);
+	}
+	putchar('\n');
+	return (0);
+}
