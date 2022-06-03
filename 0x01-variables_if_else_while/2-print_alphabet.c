64122bed550fdd4e975b77b98a5964ab50df7f9b
Akinola <akinolatofunmi04@gmail.com>
2022-06-03 15:37:27 -0700

    Test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	alpha = 'a';
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	return (0);
+}
+	
