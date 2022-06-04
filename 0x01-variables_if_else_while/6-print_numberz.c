76e6bb74179afdde7f5dffb53ae46250e2f36cfd
Akinola <akinolatofunmi04@gmail.com>
2022-06-04 00:44:27 -0700

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decimal;
+
+	decimal = 0;
+	for (decimal = 0; decimal < 10; decimal++)
+		putchar(decimal);
+	putchar('\n');
+	return (0);
+}
+	
