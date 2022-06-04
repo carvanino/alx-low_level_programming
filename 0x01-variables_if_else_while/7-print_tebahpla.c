0d5310e5f11211e79296e06ffe148ab9d4a3ba50
Akinola <akinolatofunmi04@gmail.com>
2022-06-04 01:27:14 -0700

    Task 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char rev_alpha;
+
+	rev_alpha = 'a';
+	for (rev_alpha = 'a'; rev_alpha <= 'z'; rev_alpha--);
+	{
+		putchar(rev_alpha);
+	}
+	putchar('\n');
+	return (0);
+}
