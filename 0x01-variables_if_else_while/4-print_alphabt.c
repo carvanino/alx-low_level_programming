00e66485746565152400f7fd8033fcabeeaeb5b7
Akinola <akinolatofunmi04@gmail.com>
2022-06-03 21:45:17 -0700

    Twst

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char alph_a;
+
+	for (alph_a = 'a'; alph_a <= 'z'; alph_a++)
+		putchar(alph_a);
+	 while (alph_a == e && q)
+	 {
+		 putchar('\n');
+	 }
+	 return (0);
+}
