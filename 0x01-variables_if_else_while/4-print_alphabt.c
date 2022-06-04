f1879892a24ef442d39499bff20edc8b4cb7d970
Akinola <akinolatofunmi04@gmail.com>
2022-06-03 22:15:40 -0700

    Test

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,15 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char alph_a;
 
 	for (alph_a = 'a'; alph_a <= 'z'; alph_a++)
 	if (alph_a != 'e' && alph_a != 'q')
-	 {
+	{
 		 putchar(alph_a);
-	 }
+	}
 	 putchar('\n');
-	 return (0);
+	return (0);
 }
