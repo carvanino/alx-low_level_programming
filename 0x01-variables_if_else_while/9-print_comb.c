1c82be954a4b05790ef4f257f09b3f4c144f98f4
Akinola <akinolatofunmi04@gmail.com>
2022-06-04 05:08:26 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,8 @@ int main(void)
 	num = '0';
 	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num,',');
-		if (num <= '8');
+		putchar(num);
+		if (num <= '8')
 		{
 			putchar(',');
 			putchar(' ');
