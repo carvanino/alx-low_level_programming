de6f60fe102eb768e071f3de7fc8c9b917d3c32e
Akinola <akinolatofunmi04@gmail.com>
2022-06-07 08:31:48 -0700

    Test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/** 
+/**
  * main - prints _putchar
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,7 +15,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r');	
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
