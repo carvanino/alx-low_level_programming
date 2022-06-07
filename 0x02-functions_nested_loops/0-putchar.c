248cf22d2fa80ad8fd00aeba95d37db836b9c149
Akinola <akinolatofunmi04@gmail.com>
2022-06-07 06:56:50 -0700

    Test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,10 @@
  */
 
 int main(void)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char name = "_putchar";
+	putchar(name);
 	_putchar('\n');
 
 	return(0);
