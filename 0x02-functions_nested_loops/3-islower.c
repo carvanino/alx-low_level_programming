7d89f1575dd99952314fd3730da9d7042068c11f
Akinola <akinolatofunmi04@gmail.com>
2022-06-07 16:42:29 -0700

    Task 3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,16 @@
 #include <ctype.h>
 
 /**
- * int _islower(int c) - checks if character input is lowercase or not
+ * _islower - checks if character input is lowercase or not
  *
- * Return: 1 on Success and 0 on failure 
+ * @c: character type letter
+ * Return: 1 on Success and 0 on failure
  */
+
 int _islower(int c)
 {
 	int lower;
+
 	lower = _islower(c);
 
 	if (lower > 0)
