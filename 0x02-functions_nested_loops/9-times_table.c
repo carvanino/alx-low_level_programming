485fcd6662cc90b0b96f4026e51310678958e8e6
Akinola <akinolatofunmi04@gmail.com>
2022-06-08 15:34:53 -0700

    Task 9. Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,13 @@ void times_table(void)
 {
 	int num;
 	int num_;
-	int t;
 
 	for (num = '0'; num <= '9'; num++)
 	{
 		for (num_ = '0'; num_ <= '9'; num++)
 		{
 			_putchar(num);
-			_putchar(*);
+			_putchar('*');
 			_putchar(num_);
 			_putchar(',');
 			_putchar(' ');
