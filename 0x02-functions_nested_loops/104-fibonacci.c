bb91c9b4fb8173279a119232aafbd214b9e4e467
Akinola <akinolatofunmi04@gmail.com>
2022-06-08 14:28:40 -0700

    Taak 16. First 98 Fibo num starting from 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,28 +9,28 @@
 
 int main(void)
 {
-	int amt;
+	int a;
 	unsigned long int fibo;
 	unsigned long int n1;
 	unsigned long int n2;
 
-	amt = 0;
+	a = 0;
 	n1 = 0;
 	n2 = 1;
 	fibo = 0;
 
-	while (amt < 99)
+	while (amt < 98)
 	{
 		fibo = n1 + n2;
 		n1 = n2;
 		n2 = fibo;
 		printf("%lu", fibo);
 
-		if (amt < 98)
+		if (amt < 97)
 		{
 			printf(", ");
 		}
-		amt++;
+		a++;
 	}
 	putchar('\n');
 	return (0);
