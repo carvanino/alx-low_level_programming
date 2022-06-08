9d11863cde72a1e9d857f4fd0b9f64d743b3991a
Akinola <akinolatofunmi04@gmail.com>
2022-06-08 03:05:10 -0700

    Task 9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table from 0 to 12
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void times_table(void)
+{
+	int num;
+	int num_;
+	int t;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		for (num_ = '0'; num_ <= '9'; num++)
+		{
+			t = num * num_;
+			_putchar(t);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
