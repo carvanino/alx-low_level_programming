452b4e8bde13e8ef9e188b85ed48d19b8c43327c
Akinola <akinolatofunmi04@gmail.com>
2022-06-07 17:51:49 -0700

    Task 4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * Return: 1 if Success and 0 for failure
+ *
+ * @c: character type
+ */
+
+int _isalpha(int c)
+{
+	int low_alpha;
+	int up_alpha;
+
+	low_alpha = 'a';
+	up_alpha = 'A';
+	while (low_alpha <= 'z' && up_alpha <= 'Z')
+	{
+		if (c == low_alpha || c == up_alpha)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
