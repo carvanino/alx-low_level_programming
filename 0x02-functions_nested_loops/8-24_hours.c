bfdea2e872dd0b916dec96a31aafd18a9346abf9
Akinola <akinolatofunmi04@gmail.com>
2022-06-08 02:15:33 -0700

    Task 8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,6 +15,10 @@ void jack_bauer(void)
 	{
 		for (h2 = '0'; h2 <= '9'; h2++)
 		{
+			if (h1 == 2 && h2 == '3')
+			{
+				break;
+			}
 			for (m1 = '0'; m1 <= '5'; m1++)
 			{
 				for (m2 = '0'; m2 <= '9'; m2++)
