4965cdc1d6e47ad5e0806818cddb15d4a50b5830
Akinola <akinolatofunmi04@gmail.com>
2022-06-08 04:56:11 -0700

    Task 11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		printf("%d", n);
+		printf("%d, ", n);
 		n++;
 	}
 	else if (n > 98)
 	{
-		printf("%d", n);
+		printf("%d, ", n);
 		n--;
 	}
-	printf("%d", n);
+	printf("%d, ", n);
 	printf("\n");
 }
