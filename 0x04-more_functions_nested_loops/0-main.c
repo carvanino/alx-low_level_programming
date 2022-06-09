196c6e82a400fb105e5160584f1db689f870d6c9
Akinola <akinolatofunmi04@gmail.com>
2022-06-09 15:28:59 -0700

    fixed

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 (success), 0 (fail)
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-print_most_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
