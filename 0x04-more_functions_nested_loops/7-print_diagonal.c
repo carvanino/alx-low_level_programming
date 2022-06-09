24ccb83bd9a21a524cfbda8610371a4cbe74258d
Akinola <akinolatofunmi04@gmail.com>
2022-06-09 13:09:49 -0700

    task fix

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: character representing length of line
+ *
+ * Return: Always 0.Success
+ */
+void print_diagonal(int n)
+{
+	n = 0;
+
+	while (n > 1)
+	{
+		_putchar
