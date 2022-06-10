a8bc5064514e7bf076c88381ad7164566314b27c
Akinola <akinolatofunmi04@gmail.com>
2022-06-09 20:46:35 -0700

    Task 10 Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints character using character #
+ *
+ * @size: rep. size of triangle to be printed
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int c = 0, i,  n = size - 1;
+
+	if (size > 0)
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				if (i < n)
+				{
+					_putchar(' ')
+				}
+				else
+				{
+					_putchar('#');
+				}
+				n--;
+				_putchar('\n')
+			}
+		}
+		else
+			_putchhar('\n');
+	}
+}
