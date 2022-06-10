b72a58566c1333909c61f6abfc5aeb3f02d9de7e
Akinola <akinolatofunmi04@gmail.com>
2022-06-09 18:55:00 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints number 1 to 100 ommitting multiples of 3 and 5 
+ * main - prints number 1 to 100 ommitting multiples of 3 and 5
  *
  * Return: Always 0 (Success)
  *
@@ -11,18 +11,18 @@ int main(void)
 {
 	int num;
 
-	num = 1; 
+	num = 1;
 	while (num <= 100)
 	{
-		if (num % 3 = 0)
+		if (num % 3 ==  0)
 		{
 			printf("Fizz ");
 		}
-		else if (num % 5 = 0)
+		else if (num % 5 == 0)
 		{
 			printf("Buzz ")
 		}
-		else if (num % 3 = 0 && num % 5 = 0)
+		else if (num % 3 == 0 && num % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
