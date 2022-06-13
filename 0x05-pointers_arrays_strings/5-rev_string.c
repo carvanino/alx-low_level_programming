91c090e36244e682865cc7bdf9bb3e67e0e269af
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 15:00:57 -0700

    Task5 check 1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ *
+ * @s: string thag will be reversed
+ *
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		return (s[i]);
+	}
+}
