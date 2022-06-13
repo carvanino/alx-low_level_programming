a0a38356914bf78f5cd5aa0fe6e979e986063677
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 16:04:12 -0700

    Task 6 Check 1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character starting with the first
+ *
+ * @str: Steing to be printed in that manner
+ *
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i % 2 == 0)
+		{
+			continue;
+		}
+		_putchar(str[i]);
+	}
+}
