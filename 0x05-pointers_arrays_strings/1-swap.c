e7288009afe2b164a9c63bd79030bcb27d3c8770
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 10:08:15 -0700

    Check

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,13 +4,16 @@
  * swap_int - swaps the value of two integers
  *
  * @a: fist integer
- * @b: Second integer 
+ * @b: Second integer
  *
  * Return: Always 0
  */
 
 void swap_int(int *a, int *b)
 {
-	a = &b;
-	b = &a;
+	int c;
+	int d;
+
+	a = &d;
+	b = &c;
 }
