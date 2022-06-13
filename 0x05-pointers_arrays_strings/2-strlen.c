07aec340313dc0d85a7332414d2a20d5cf0cf9bc
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 10:47:39 -0700

    check

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: Character length to return
+ *
+ * Return: 0 Always
+ */
+
+int _strlen(char *s)
+{
+	sizeof(s);
+}
