c210f853b7fdba062e473035cfb307016deb753b
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 17:37:56 -0700

    Task 7 check 1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string with conditions
+ *
+ * @str: String to be printed in half
+ *
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int i, len;
+
+	for (len = 0; str[len]; len++)
+	{
+		i = len / 2;
+		for (; str[i]; i++)
+		{
+			_putchar(str[i]);
+			n = (len - 1) / 2;
+
+			if ((len / 2) != 0)
+			{
+				_putchar(str[n]);
+			}
+		}
+	}
+	putchar('\n');
+}
