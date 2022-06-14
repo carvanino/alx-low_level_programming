8766f36c4907858c5f941b2da7631a876be1cc4e
Akinola <akinolatofunmi04@gmail.com>
2022-06-13 21:30:15 -0700

    Task 8 submit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n element array of integers
+ *
+ * @a: Integers to be in array
+ * @n: Integers to be printed in array
+ *
+ * Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
