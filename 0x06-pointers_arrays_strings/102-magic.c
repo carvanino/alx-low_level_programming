8761bcff0d3651f293faf6236a51e48c818d54f4
Akinola <akinolatofunmi04@gmail.com>
2022-06-19 18:02:50 -0700

    Task 10 Submit Check 1

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write yoir line of code here...
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
@@ -18,6 +18,6 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = d\n", a[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
