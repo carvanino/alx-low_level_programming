7e341e108fd9788177c06b988a586c978f34b051
Akinola <akinolatofunmi04@gmail.com>
2022-06-20 10:06:40 -0700

    Task 5 check

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: main string
+ * @needle: substring to be located in string
+ *
+ * Return: a poubter to the beginning of substring in string (success)
+ * NULL (fail)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++, i++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				return (haystack + (i + j) + 1);
+			}
+		}
+	}
+	return (0);
+}
