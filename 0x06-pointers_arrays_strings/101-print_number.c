32b5038423d465fb4dfc6b516241f2725233b518
Akinola <akinolatofunmi04@gmail.com>
2022-06-19 17:10:20 -0700

    Task 9, check 2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,18 +10,20 @@
 
 void print_number(int n);
 {
+	unsigned int n_ = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n_ = n_ * -1;
 	}
-	if (n == 0)
+	if (n_ == 0)
 	{
 		_putchar('0');
 	}
-	if (n / 10)
+	if (n_ / 10)
 	{
-		print_number(n / 10);
+		print_number(n_ / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(n_ % 10 + '0');
 }
