ed671be78dfaf061ee52eb50ca0857a631d8d00c
Akinola <akinolatofunmi04@gmail.com>
2022-06-24 05:34:50 -0700

    submit Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string to be converted
+ *
+ * Return: 0 string doesnt contain number, 1 otherwise.
+ */
+
+int _atoi(char *s)
+{
+	int a = 1;
+	int i;
+	int r;
+
+	for (i = 0; !(s[i] >= 48 && s[i] <= 57); i++)
+	{
+		if (s[i] == '-')
+		{
+			a = a * -1;
+		}
+	}
+	for (i = 0; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		r = r * 10;
+		r = r + (s[i] - 48);
+	}
+	return (a * r);
+}
