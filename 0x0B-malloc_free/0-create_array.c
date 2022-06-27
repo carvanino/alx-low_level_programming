a2e9771930b7acafaae9210112fc47c3f57b898c
Akinola <akinolatofunmi04@gmail.com>
2022-06-27 12:24:44 -0700

    Task 0 check 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates array of char & initializes it with a specific char
+ *
+ * @size: size of array
+ * @c: character to initialize
+ *
+ * Return: 0 if (Fail); NULL if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
