8fa8166aed823e8402eff1a0634972e092194529
Akinola <akinolatofunmi04@gmail.com>
2022-06-27 18:13:28 -0700

    Task 0 - Fix malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
+	ptr = (char *)malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (0);
 	}
-	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
