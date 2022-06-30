e9f6eac22282a49db47e4de11e4a2ac82f0df4ad
Akinola <akinolatofunmi04@gmail.com>
2022-06-30 16:10:41 -0700

    Fix 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (catstr == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < len_s1; i++)
 	{
 		catstr[i] = s1[i];
 	}
