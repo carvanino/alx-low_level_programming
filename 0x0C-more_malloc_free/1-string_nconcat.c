50c0b50ec38e9f25a86775dc46b89b46c8485274
Akinola <akinolatofunmi04@gmail.com>
2022-06-30 21:49:34 -0700

    correct submit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (catstr == NULL)
 		return (NULL);
 	len = 0;
-	for (i = 0; s[i]; i++)
+	for (i = 0; s1[i]; i++)
 	{
 		catstr[len++] = s1[i];
 	}
