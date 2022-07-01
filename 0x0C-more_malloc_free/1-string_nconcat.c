e2d013b74f46659682c3d64b663f455c3e0470e6
Akinola <akinolatofunmi04@gmail.com>
2022-06-30 21:47:31 -0700

    submit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,38 +13,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-       	unsigned int len_s1, len_s2, i;
+	unsigned int len = n, i;
 	char *catstr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len_s1 = 0; s1[len_s1]; len_s1++)
+	for (i = 0; s1[i]; i++)
 	{
-	}
-	for (len_s2 = 0; s2[len_s2]; len_s2++)
-	{
+		len++;
 	}
 
-	if (n >=  len_s2)
-	{
-		catstr = malloc(sizeof(char) * (len_s1 + n  +  1));
-	}
-	else
-	{
-		catstr = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	}
+	catstr = malloc(sizeof(char) * (len +  1));
 	if (catstr == NULL)
 		return (NULL);
-	for (i = 0; i < len_s1; i++)
+	len = 0;
+	for (i = 0; s[i]; i++)
 	{
-		catstr[i] = s1[i];
+		catstr[len++] = s1[i];
 	}
 	for (i = 0; s2[i] && i < n; i++)
 	{
-		catstr[len_s1 + i] = s2[i];
+		catstr[len++] = s2[i];
 	}
-	catstr[i] = '\0';
+	catstr[len] = '\0';
 	return (catstr);
 }
