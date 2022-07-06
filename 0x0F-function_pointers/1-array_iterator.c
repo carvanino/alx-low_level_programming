79afc7670aae19907be126ba97c9f56331ad8ae1
Akinola <akinolatofunmi04@gmail.com>
2022-07-06 11:59:27 -0700

    fixs datatype comparison

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
-	if (array == NULL || size == NULL || action == NULL)
+	if (array == NULL ||  action == NULL)
 	{
 		return;
 	}
