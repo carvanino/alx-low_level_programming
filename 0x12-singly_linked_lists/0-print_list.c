a70d62cad8884abac8f55b1f9d6effd61634d695
Akinola <akinolatofunmi04@gmail.com>
2022-07-14 14:11:30 -0700

    Fix Betty and Code arrangement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0; 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
 		h = h->next;
 	}
