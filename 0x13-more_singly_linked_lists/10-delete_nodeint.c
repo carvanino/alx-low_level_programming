056634ab8100ea7a5c07d5d2695abf4d88685f61
Akinola <akinolatofunmi04@gmail.com>
2022-07-19 03:52:01 -0700

    Task 10 - A function that deletes a node from a particular position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at index of a list
+ *
+ * @head: pointer to the beginning of the list
+ * @index: position of node to be deleted
+ *
+ * Return: -1 if Fail
+ * Otherwise: 1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *trans_v, *prevnode;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	trans_v = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(trans_v);
+		return (-1);
+	}
+	while (i < index)
+	{
+		if (!trans_v || !(trans_v->next))
+		{
+			return (-1);
+		}
+		prevnode = trans_v;
+		trans_v = trans_v->next;
+		i++;
+	}
+	prevnode->next = trans_v->next;
+	free(trans_v);
+
+	return (1);
+}
