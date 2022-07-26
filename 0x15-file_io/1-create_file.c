e8d0d76dd25696cf8a30626dbf63c0331d324a82
Akinola <akinolatofunmi04@gmail.com>
2022-07-25 17:37:14 -0700

    Fix Betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - creates a file
  *
  * @filename: pointer to the pathname
- * @text_content - pointer to the content of the file created
+ * @text_content: pointer to the content of the file created
  * Return: 1 on Success and -1 on fail
  */
 
@@ -14,8 +14,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
