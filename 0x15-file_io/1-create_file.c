8f2c4705d5834bdb819112534a7626caaa6c9b99
Akinola <akinolatofunmi04@gmail.com>
2022-07-25 17:26:17 -0700

    Task 1- A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: pointer to the pathname
+ * @text_content - pointer to the content of the file created
+ * Return: 1 on Success and -1 on fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, count;
+
+	if (filename == NULL)
+	{
+		return(-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		for (count = 0; text_content[count]; count++)
+		{
+		}
+		w = write(fd, text_content, count);
+		if (w == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
