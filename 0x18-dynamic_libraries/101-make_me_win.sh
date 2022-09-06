62c018620fdb05b3d44935d1e47ddd62bd33a928
Akinola <akinolatofunmi04@gmail.com>
2022-09-05 17:13:30 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/carvanino/alx-low_level_programming/raw/main/0x18-dynamic_libraries/winning_ticket.so
+wget -P /tmp https://github.com/carvanino/alx-low_level_programming/blob/fa0812669edb8c52188d6b5a0dcfa360f146087d/0x18-dynamic_libraries/winning_ticket.so
 export LD_PRELOAD=/tmp/winning_ticket.so
