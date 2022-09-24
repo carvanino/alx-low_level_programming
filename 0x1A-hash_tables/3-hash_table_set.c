6fd8247e10db813eb95f3820a5af0ae3f62d0bcb
Akinola <akinolatofunmi04@gmail.com>
2022-09-23 20:52:22 -0700

    A function was better off

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,33 @@
 #include "hash_tables.h"
 
 /**
+ * check_key - Checks a particulatr index array to see if the newly added
+ * key already exist
+ *
+ * @node: Index of array
+ * @key: The key to check
+ * @value: The value
+ *
+ * Return: 1 if Success
+ * Otherwise 0
+ */
+
+int check_key(hash_node_t *node, char *key, char *value)
+{
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = value;
+			return (1);
+		}
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table
  *
  * @ht: The hash table
@@ -25,17 +52,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_cpy == NULL || key_cpy == NULL)
 		return (0);
 
-	while (ht->array[index] != NULL)
+	if (check_key(ht->array[index], key_cpy, value_cpy) == 1)
 	{
-		if (strcmp(ht->array[index]->key, key_cpy) == 0)
-		{
-			free(ht->array[index]->value);
-			ht->array[index]->value = value_cpy;
-			return (1);
-		}
-		ht->array[index] = ht->array[index]->next;
+		return (1);
 	}
-
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
