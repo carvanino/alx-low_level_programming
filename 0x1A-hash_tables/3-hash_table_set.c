b0d25d93fd87bd1365827a2add901878c383f38f
Akinola <akinolatofunmi04@gmail.com>
2022-09-23 20:38:58 -0700

    ...

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, i;
+	unsigned long int index;
 	hash_node_t *new_node;
 	char *value_cpy, *key_cpy;
 
@@ -25,32 +25,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_cpy == NULL || key_cpy == NULL)
 		return (0);
 
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key_cpy) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = value;
+			return (1);
+		}
+		ht->array[index] = ht->array[index]->next;
+	}
+
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = key_cpy;
 	new_node->value = value_cpy;
 
-	/*
-	 * First, we check through the array if a key already exist,
-	 * and we update its value, instead of making a duplicate
-	 */
-	for (i = 0; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key_cpy) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_cpy;
-			return (1);
-		}
-	}
-	/* If there is collision: Array index has already been used */
 	if (ht->array[index] != NULL)
 	{
 		new_node->next = ht->array[index];
 	}
 	else
+	{
 		new_node->next = NULL;
+	}
 	ht->array[index] = new_node;
 	return (1);
 }
