cfa362862bd6312b5292863e2ad63b0efa58ea7b
Akinola <akinolatofunmi04@gmail.com>
2023-01-03 09:58:22 +0000

    Task 7 - Jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,63 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using jump search
+ * Uses the square root of the size of the array as the jump step
+ *
+ * @array: array to retrieve value from
+ * @size: size of the array
+ * @value: value to be retrieved from the array
+ *
+ * Return: The index of value in the array
+ * Otherwise: -1
+ */
+
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, temp;
+	int jump;
+
+	jump = square_root(size);
+	for (i = 0; i < size && array[i] < value; i += jump)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	/*printf("%d\n", array[i]);*/
+	temp = i;
+	i = i - jump;
+	printf("Value found between indexes [%ld] and [%ld]\n", i, temp);
+	/*if (abs(array[i]) >= value && i > 0)*/
+	/*{*/
+	/*temp = i;
+	i = i - jump;*/
+	/*printf("value of temp is %d\n", temp);*/
+	for (i = i; i <= temp && abs(array[i]) <= value; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	/*}*/
+	return (-1);
+}
+
+
+int square_root(int number)
+{
+	int range = number / 4;
+	int i = 0;
+
+	for (i = 0; i <= range; i++)
+	{
+		if (i * i == number)
+		{
+			return (i);
+		}
+	}
+	return (i);
+}
